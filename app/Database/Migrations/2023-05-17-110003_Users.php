2550d52ce97b1b1abd06885a4f47e49fb507839e
BubbleJizun <muhammadajiz975@gmail.com>
2023-05-17 18:49:28 +0700

    commit pertama

---
+++ app/Database/Migrations/2023-05-17-110003_Users.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'BIGINT',
+                'constraint'     => 10,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '150',
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '150',
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '150',
+            ],
+            'role' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '20',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
---
+++ app/Models/Users.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Users extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'users';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+}
