efb5444d7bc829658eea116a218615d7f1b5b57d
Muhammad Ajiz Alfarizi <muhammadajiz975@gmail.com>
2023-05-23 15:10:43 +0700

    🔒 api: edit Diskusi Model

--- app/Models/DiskusiModel.php
+++ app/Models/DiskusiModel.php
@@ -6,36 +6,16 @@ use CodeIgniter\Model;
 
 class DiskusiModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'diskusis';
-    protected $primaryKey       = 'id';
+    protected $table            = 'diskusi';
     protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $returnType       = 'object';
+    protected $allowedFields    = [
+        'id', 'title', 'slug', 'desk', 'user_id', 'created_at', 'updated_at'
+    ];
 
     // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'int';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
